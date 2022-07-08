4a75ec3b811d5f47a71397f9c1440f0b25cb74bf
NRG1989 <onezhkin@gmail.com>
2022-07-08 22:09:08 +0300

    7

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"ws-server/internal/handlers"
 
 	"github.com/bmizerany/pat"
 )
 
-func routes() http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Home))
-	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
-	return mux
-}
-
 func main() {
 	mux := routes()
-	log.Println("Starting web server on port 8080")
-	http.ListenAndServe(":8080", mux)
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Staring web server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+}
+
+// routes defines the application routes
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	return mux
 }
